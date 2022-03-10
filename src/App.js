18a8549d3715bc20c9616cc291f05515716ac25c
Shahbaaz <shahbaazc@gmail.com>
2022-03-10 12:34:01 +0530

    using async/await with arrow function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-  const [movies, setMovie] = useState([])
+  const [movies, setMovie] = useState([]);
 
-   const fetchMovie = () => {
-     fetch('https://swapi.py4e.com/api/films/').then(res => {
-       return res.json();
-     }).then(data => {
-      const transformedMovie = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        };
-      });
-      setMovie(transformedMovie)
-     })
-   }
+  const fetchMovie = async () => {
+    const response = await fetch("https://swapi.py4e.com/api/films/");
+    const data = await response.json();
+    const transformedMovie = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovie(transformedMovie);
+  };
 
   return (
     <React.Fragment>
@@ -28,7 +26,7 @@ function App() {
         <button onClick={fetchMovie}>Fetch Movies</button>
       </section>
       <section>
-        {movies.length == 0 && <MoviesList movies={movies} /> }
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
