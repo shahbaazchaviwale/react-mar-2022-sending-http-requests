d247b954b211fe913c752fbbd3def14cc2e4cb21
Shahbaaz <shahbaazc@gmail.com>
2022-03-10 12:46:31 +0530

    Handling Loading & Data States

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovie] = useState([]);
+  const [isloading, setIsloading] = useState(false);
 
   const fetchMovie = async () => {
+    setIsloading(true);
     const response = await fetch("https://swapi.py4e.com/api/films/");
     const data = await response.json();
     const transformedMovie = data.results.map((movieData) => {
@@ -18,6 +20,7 @@ function App() {
       };
     });
     setMovie(transformedMovie);
+    setIsloading(false);
   };
 
   return (
@@ -26,7 +29,9 @@ function App() {
         <button onClick={fetchMovie}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isloading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isloading && movies.length == 0 && <h3>No movie found</h3>}
+        {isloading && <h3>Loading...</h3>}
       </section>
     </React.Fragment>
   );
