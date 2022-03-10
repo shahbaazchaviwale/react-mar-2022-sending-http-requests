206e6fe2b56f08b6e67e332423273b0c6f36fe1b
Shahbaaz <shahbaazc@gmail.com>
2022-03-10 12:22:08 +0530

    sending a GT request

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-  
+  const [movies, setMovie] = useState([])
+
+   const fetchMovie = () => {
+     fetch('https://swapi.py4e.com/api/films/').then(res => {
+       return res.json();
+     }).then(data => {
+      const transformedMovie = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        };
+      });
+      setMovie(transformedMovie)
+     })
+   }
+
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMovie}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {movies.length == 0 && <MoviesList movies={movies} /> }
       </section>
     </React.Fragment>
   );
