ed6fc9dad0dd53556261805323bbbc8fcc2b5284
Shahbaaz <shahbaazc@gmail.com>
2022-03-10 11:58:56 +0530

    add files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  
+  return (
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
